de37a0f32d2da56b84e3eaa676948918752463bf
Derek Morris <dmow4454@gmail.com>
2013-07-01 13:57:23 -0400

    New HTML table

---
+++ programs/HTML.py
@@ -0,0 +1,24 @@
+table = "<table border=1>\n"
+
+item = 1
+
+for row in range(0,10):
+    table = table + "  <tr>"
+
+    for col in range(0,10):
+        table = table + "<td>"
+        if item <= 97:
+            table = table + "%i" % item
+        item = item + 1
+        table = table + "</td>"
+
+    table = table + "</tr>\n"
+
+table = table + "</table>"
+
+f = open ('abc.html', 'w')
+print >>f, table
+
+import webbrowser
+handle = webbrowser.get()
+handle.open('abc.html')
