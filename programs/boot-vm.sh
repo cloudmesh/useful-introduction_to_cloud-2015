c7b2b369dd55810ef8aad6e4bb43f53cdb8d20de
Fugang Wang <kevinwangfg@gmail.com>
2014-09-19 12:24:35 -0400

    fixing the shell script to boot vm

--- programs/boot-vm.sh
+++ programs/boot-vm.sh
@@ -1,7 +1,9 @@
 #! /bin/sh -x
 
 USERNAME=$USER
+nova keypair-add --pub-key ~/.ssh/id_rsa.pub $USERNAME-india-key
 
 nova boot --flavor m1.small \
             --image "futuregrid/ubuntu-14.04" \
-            --key_name ~/.ssh/idrsa.pub $USERNAME-001
+            --key_name $USERNAME-india-key $USERNAME-001
+
