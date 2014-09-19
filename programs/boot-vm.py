15a210681b1be0d0a97adf71fa758cf3ffc56fd4
Fugang Wang <kevinwangfg@gmail.com>
2014-09-19 12:41:27 -0400

    python and shell hybrid model to start a vm

--- programs/boot-vm.py
+++ programs/boot-vm.py
@@ -1,6 +1,16 @@
-from sh import nova 
-username = "gregor"
+#!/uer/bin/env python
 
-print nova ("flavor-list")
-print nova ("boot", "--flavor", "m1.small", "--image", "futuregrid/ubuntu-12.04", "--key_name", "~/.ssh/id_rsa.pub", username + "001")
+import os
+#from sh import nova 
 
+homedir = os.path.expanduser("~/")
+username = homedir.split("/")[-2]
+
+# to avoid installing sh
+#print nova ("flavor-list")
+
+#print nova ("keypair-add", "--pub-key", "~/.ssh/id_rsa.pub" "%s-india-key" % username)
+#print nova ("boot", "--flavor", "m1.small", "--image", "futuregrid/ubuntu-12.04", "--key_name", "%s-india-key" % username, username + "001")
+
+os.system("nova keypair-add --pub-key ~/.ssh/id_rsa.pub {0}-india-key".format(username) )
+os.system('nova boot --flavor m1.small --image "futuregrid/ubuntu-14.04" --key_name {0}-india-key {0}-001'.format(username) )
