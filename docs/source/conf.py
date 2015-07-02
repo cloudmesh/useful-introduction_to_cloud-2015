5eff423db7bf93bc1a78ada3081e93dc18fabc92
Hyungro Lee <hroe.lee@gmail.com>
2015-07-02 15:45:16 +0000

    disable autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ elif kr:
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [#'sphinx.ext.autodoc',
               'sphinx.ext.doctest',
               'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
